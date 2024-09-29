21b0a04637c85bc7827525b1512f829dbc7425c7
Rene Krizak <rene.krizak@gmail.com>
2024-09-29 22:47:51 +0200

    p4 fix

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,60 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+@SCREEN
+D=A
+
+// reads kbd input -> if > 0 -> fill black, else fill white
+(CHECKPRESS)
+    @KEYBOARD
+    D=M           
+    @FILLBLACK
+    D;JGT         // if input > 0 goto FILLBLACK
+    @FILLWHITE
+    0;JMP         // else goto FILLWHITE
+
+// Fill the screen with black (-1)
+(FILLBLACK)
+    @16384        // base addr of screen, hack platform has 8192 screen size
+    D=A           
+    @PIXELS
+    M=D           
+    @24576        
+    D=A           // D = 24576 -> end of screen
+
+(FILLBLACK_LOOP)
+    @PIXELS
+    A=M           // A -> current addr of pixel
+    M=-1          // set clr to black
+    @PIXELS
+    M=M+1         // addr++ until end of cscreen
+    @PIXELS
+    D=M
+    @24576
+    D=D-A    
+    @FILLBLACK_LOOP
+    D;JLT         //keep looping until we reach end of screen, if we do go back to checkpress
+    @CHECKPRESS   
+
+//works the same just as fillblack & fillblack_loop
+(FILLWHITE)
+    @16384        
+    D=A           
+    @PIXELS
+    M=D           
+    @24576        
+    D=A           
+
+(FILLWHITE_LOOP)
+    @PIXELS
+    A=M           
+    M=0           
+    @PIXELS
+    M=M+1         
+    @PIXELS
+    D=M
+    @24576
+    D=D-A       
+    @FILLWHITE_LOOP
+    D;JLT         
+    @CHECKPRESS   
--- projects/4/fill/example.asm
+++ projects/4/fill/example.asm
@@ -46,3 +46,47 @@
 (END)   
     @END
     0;JMP 
+
+
+@color    // declare color variable
+M=0      // by default is white
+
+(LOOP)
+
+  @SCREEN
+  D=A
+  @pixels
+  M=D         // pixel address, goes from 16384 to 16384 + 8192 == 24576
+
+  @KBD    // keyboard address
+  D=M
+  @BLACK
+  D;JGT     // if(keyboard > 0) goto BLACK
+  
+  @color
+  M=0       // set to white
+  @COLOR_SCREEN
+  0;JMP     // jump to subroutine that colors the screen
+  
+  (BLACK)
+    @color
+    M=-1    // set to black (2's complement 111111111...)
+
+  (COLOR_SCREEN)
+    @color
+    D=M
+    @pixels
+    A=M         // VERY IMPORTANT! indirect address
+    M=D         // color M[pixels] with @color
+    
+    @pixels
+    M=M+1
+    D=M
+        
+    @24576
+    D=D-A
+    @COLOR_SCREEN
+    D;JLT
+
+@LOOP
+0;JMP // infinite loop
